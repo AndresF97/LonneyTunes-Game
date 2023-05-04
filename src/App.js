fab8326825dbe310d04367eaf129376a72be55ca
AndresF97 <Andres.F.Jimenez.F@gmail.com>
2023-05-05 06:19:50 +0900

    clicked functionality worked and fixed styling bug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import Navbar from "./components/Navbar"
 import HomePage from './components/pages/HomePage';
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,29 @@
 import React from "react";
 import lonneyCards from "./looney.json"
 
+// TO DO:
+// CREATE A TIMER
+// SCORING SYSTEM
+// CREATE TWO RANDOMISE ARRAYS
+// ADD STYLING FOR EACH CARD
+// Future developement:
+// PvP (take turns and time)
+// Single player
+// AI chatgpt or another ai that can be implemented in React
 
 
 function Card(){
-   const clickedCard =()=>{
-        console.log("Clicked")
+   const clickedCard =(event)=>{
+        
+        console.log(event.target)
     }
 
-    console.log(lonneyCards)
     return (
         <>
         <h1>Single Card</h1>
         {lonneyCards.map(item=>{
             return(
-            <section onClick={()=>{clickedCard()}}>
+            <section onClick={(event)=>{clickedCard(event)}} data-cardname={item.name} data-cardid={item.id}>
                 <h1>{item.name}</h1>
                 <img src={`${item.image}`}/>
             </section>
