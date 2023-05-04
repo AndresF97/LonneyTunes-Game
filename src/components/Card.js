73b9943d2e2c7ad561dd77942a0d28af65121391
AndresF97 <Andres.F.Jimenez.F@gmail.com>
2023-05-04 23:52:52 +0900

    Images render with clicked function hooked on each item weird bug with centered imtems

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,10 +4,22 @@ import lonneyCards from "./looney.json"
 
 
 function Card(){
+   const clickedCard =()=>{
+        console.log("Clicked")
+    }
+
     console.log(lonneyCards)
     return (
         <>
         <h1>Single Card</h1>
+        {lonneyCards.map(item=>{
+            return(
+            <section onClick={()=>{clickedCard()}}>
+                <h1>{item.name}</h1>
+                <img src={`${item.image}`}/>
+            </section>
+            )
+        })}
         </>
     )
 }
