eb97a2578fe07798ea972d97f208b29ecf4a2143
AndresF97 <Andres.F.Jimenez.F@gmail.com>
2023-05-06 09:44:15 +0900

    finally got cards to render randomize

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import lonneyCards from "./looney.json"
 
 // TO DO:
@@ -12,64 +12,78 @@ import lonneyCards from "./looney.json"
 // AI chatgpt or another ai that can be implemented in React
 
 
-function Card(){
-    const [gameLogic, setGameLogic] = useState({
-        score:0,
-        randomArrOne:[],
-        randomArrTwo:[],
-    })
-    let [timer, setTimer] = useState(10); // 25 minutes
-  
-   const clickedCard =(event)=>{
-        console.log(event.target)
-        
+function Card() {
+  const [gameLogic, setGameLogic] = useState({
+    score: 0,
+    // randomArrOne: [],
+    randomArrTwo: [],
+  })
+  let [randomArrOne, setArrOne] = useState([])
+  let [randomArrTwo, setArrTwo] = useState([])
+  let [timer, setTimer] = useState(10); // 25 minutes
+
+  const clickedCard = (event) => {
+    console.log(event.target)
+
+  }
+  // const arrRandomazier = () => {
+    let clonedLoneyCards = lonneyCards
+    Array.prototype.random = function () {
+      let that = this.slice();
+      let result = []
+      this.forEach(() => {
+        result.push(that.splice(Math.floor(Math.random() * that.length), 1)[0])
+      })
+      return result
     }
-    const arrRandomazier = ()=>{
-      let clonedLoneyCards = lonneyCards
-      Array.prototype.random = function(){
-        let that = this.slice();
-        let result = []
-        this.forEach(()=>{
-          result.push(that.splice(Math.floor(Math.random()*that.length),1)[0])
-        })
-        return result
-      }
-      setGameLogic(gameLogic.randomArrOne.push(...clonedLoneyCards.random()))
-      setGameLogic(gameLogic.randomArrTwo.push(...clonedLoneyCards.random()))
-      console.log(gameLogic.randomArrOne)
-      console.log(gameLogic.randomArrTwo)
-    }
-    const startTimer = ()=>{
-      let countDown = setInterval(() => { // <-- set tick ref current value
-      if(timer < 0){
-          clearInterval(countDown)
-          return
-      }else{
-      setTimer(timer--);
-      console.log(timer)
+    // setGameLogic(gameLogic.randomArrOne.push(...clonedLoneyCards.random()))
+    // gameLogic.randomArrOne.push(...clonedLoneyCards.random())
+    // setGameLogic(gameLogic.randomArrTwo.push(...clonedLoneyCards.random()))
+    // console.log(gameLogic.randomArrOne)
+    // console.log(gameLogic.randomArrTwo)
+  // }
+  const startTimer = () => {
+    let countDown = setInterval(() => { // <-- set tick ref current value
+      if (timer < 0) {
+        clearInterval(countDown)
+        return
+      } else {
+        setTimer(timer--);
+        console.log(timer)
       }
     }, 1000);
   }
 
-    useEffect(() => {
-        // startTimer()
-        arrRandomazier();
-      }, []);
-;
-    return (
-        <>
-        <h1>Single Card</h1>
-        <div id="timer">{timer}</div>
-        {lonneyCards.map(item=>{
-            return(
-            <section key={item.id} onClick={(event)=>{clickedCard(event)}} data-cardname={item.name} data-cardid={item.id}>
-                <h1>{item.name}</h1>
-                <img src={`${item.image}`}/>
+  useEffect(() => {
+    // startTimer()
+    // arrRandomazier();
+    // let newArrOne = randomArrOne.push(...clonedLoneyCards.random())
+    
+    setArrOne(clonedLoneyCards.random())
+    setArrTwo(clonedLoneyCards.random())
+    console.log(randomArrOne)
+  }, []);
+
+  return (
+    <>
+      <h1>Single Card</h1>
+      <div id="timer">{timer}</div>
+      {lonneyCards.map((item, i) => {
+        return (
+          <>
+            <section key={item.id} onClick={(event) => { clickedCard(event) }} data-cardname={item.name} data-cardid={item.id}>
+              <h1>{randomArrOne[i]?.name}</h1>
+              <img src={`${randomArrOne[i]?.image}`} />
             </section>
-            )
-        })}
-        </>
-    )
+            <section key={item.id} onClick={(event) => { clickedCard(event) }} data-cardname={item.name} data-cardid={item.id}>
+              <h1>{randomArrTwo[i]?.name}</h1>
+              <img src={`${randomArrTwo[i]?.image}`} />
+            </section>
+          </>
+        )
+      })}
+    </>
+  )
 }
 
 export default Card;
