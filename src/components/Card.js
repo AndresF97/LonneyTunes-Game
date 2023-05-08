bd3538f341d4ecc5288e0732a726db93474f8c81
AndresF97 <Andres.F.Jimenez.F@gmail.com>
2023-05-08 15:10:19 +0900

    got background image,functionality and found a small bug with the click event

--- src/components/Card.js
+++ src/components/Card.js
@@ -14,22 +14,25 @@ import lonneyCards from "./looney.json"
 
 
 function Card() {
-  let [score, setScore] =useState(10)
+  let [score, setScore] = useState(10)
   let [randomArrOne, setArrOne] = useState([])
   let [randomArrTwo, setArrTwo] = useState([])
-  let [currentClicked,setCurrentClick] = useState('')
+  let [currentClicked, setCurrentClick] = useState('')
   let [timer, setTimer] = useState(20); // 25 minutes
 
   const clickedCard = (event) => {
-
-    setInterval(()=>{
+// issue from having same id attributes must find a way to seperate from one and other
+    setInterval(() => {
       event.target.classList.add("hide")
-    },1000)
+      event.target.parentElement.classList.add("bg-image")
+
+    }, 1000)
     event.target.classList.remove("hide")
-    
+    event.target.parentElement.classList.remove("bg-image")
+
     setCurrentClick(event.target.getAttribute('data-card-id'))
-    if(currentClicked!==''){
-      if(currentClicked === event.target.getAttribute('data-card-id') ){
+    if (currentClicked !== '') {
+      if (currentClicked === event.target.getAttribute('data-card-id')) {
         setScore(score = score + 1)
         event.target.parentElement.classList.add("destroy")
         document.querySelector(`[data-card-id="${currentClicked}"]`).parentElement.classList.add("destroy");
@@ -38,17 +41,17 @@ function Card() {
 
     }
   }
-    let clonedLoneyCards = lonneyCards
-    Array.prototype.random = function () {
-      let that = this.slice();
-      let result = []
-      this.forEach(() => {
-        result.push(that.splice(Math.floor(Math.random() * that.length), 1)[0])
-      })
-      return result
-    }
+  let clonedLoneyCards = lonneyCards
+  Array.prototype.random = function () {
+    let that = this.slice();
+    let result = []
+    this.forEach(() => {
+      result.push(that.splice(Math.floor(Math.random() * that.length), 1)[0])
+    })
+    return result
+  }
   const startTimer = () => {
-    let countDown = setInterval(() => { 
+    let countDown = setInterval(() => {
       if (timer < 0) {
         clearInterval(countDown)
         gameOver()
@@ -60,17 +63,17 @@ function Card() {
     }, 1000);
   }
 
-  const PlayAgain = () =>{
+  const PlayAgain = () => {
     window.location.reload()
   }
-  const returnHomePage = () =>{
+  const returnHomePage = () => {
     window.location.replace('/')
   }
 
-  const gameOver = ()=>{
-    document.getElementById('gameContainer').innerHTML=""
+  const gameOver = () => {
+    document.getElementById('gameContainer').innerHTML = ""
     document.getElementById('gameContainer').classList.remove('grid', 'grid-cols-2', 'md:grid-cols-7', 'gap-4', 'dark:bg-gray-900')
-    document.getElementById('gameContainer').innerHTML=`
+    document.getElementById('gameContainer').innerHTML = `
     <div className="text-center mr-5 ml-5">
     
       <h1>Game over!</h1>
@@ -78,10 +81,10 @@ function Card() {
       <button id="returnBtn" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Go back home</button>
     </div>
     `
-    document.getElementById('gameOverBtn').onclick = function(){
+    document.getElementById('gameOverBtn').onclick = function () {
       PlayAgain()
     }
-    document.getElementById('returnBtn').onclick = function(){
+    document.getElementById('returnBtn').onclick = function () {
       returnHomePage()
     }
   }
@@ -98,28 +101,30 @@ function Card() {
       <div id="timer">Timer:{timer}</div>
       <div>Score:{score}</div>
       <div className="grid grid-cols-2 md:grid-cols-7 gap-4 dark:bg-gray-900 m-5" id="gameContainer">
-      {randomArrOne?.map((item, i) => {
-        
-      
-        return (
-          <>
-            <div className="bg-red-700 rounded p-5 custom-border">
-            <section className="h-auto max-w-full text-center  hide" key={randomArrOne[i]?.i +"One"} onClick={(event) => { clickedCard(event) }} data-card-name={randomArrOne[i]?.name} data-card-id={randomArrOne[i]?.id}>
-              
-              <h1 className="text-white">{randomArrOne[i]?.name}</h1>
-              
-              <img className="m-0" src={require(`${randomArrOne[i]?.image}`)} />
-            </section>
-            </div>
-            <div className="bg-red-700 rounded p-5 custom-border">
-            <section className="h-auto max-w-full text-center hide" key={randomArrTwo[i]?.id+ "Two"} onClick={(event) => { clickedCard(event) }} data-card-name={randomArrTwo[i]?.name} data-card-id={randomArrTwo[i]?.id}>
-              <h1 className="text-white">{randomArrTwo[i]?.name}</h1>
-              <img src={require(`${randomArrTwo[i]?.image}`)} />
-            </section>
-            </div>
-          </>
-        )
-      })}
+        {randomArrOne?.map((item, i) => {
+
+
+          return (
+            <>
+              <div className="bg-red-700 rounded p-5 custom-border bg-image">
+                {/* <img  src={require('./images/looney-logo.png')}/> */}
+                <section className="h-auto max-w-full text-center  hide" key={randomArrOne[i]?.i + "One"} onClick={(event) => { clickedCard(event) }} data-card-name={randomArrOne[i]?.name} data-card-id={randomArrOne[i]?.id}>
+
+                  <h1 className="text-white">{randomArrOne[i]?.name}</h1>
+
+                  <img className="m-0" src={require(`${randomArrOne[i]?.image}`)} />
+                </section>
+              </div>
+              <div className="bg-red-700 rounded p-5 custom-border bg-image">
+                {/* <img src={require('./images/looney-logo.png')}/> */}
+                <section className="h-auto max-w-full text-center hide" key={randomArrTwo[i]?.id + "Two"} onClick={(event) => { clickedCard(event) }} data-card-name={randomArrTwo[i]?.name} data-card-id={randomArrTwo[i]?.id}>
+                  <h1 className="text-white">{randomArrTwo[i]?.name}</h1>
+                  <img src={require(`${randomArrTwo[i]?.image}`)} />
+                </section>
+              </div>
+            </>
+          )
+        })}
       </div>
     </>
   )
